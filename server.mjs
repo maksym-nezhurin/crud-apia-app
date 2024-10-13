6fec056caf196f82bc33ecfeac6cd7232fb9d8ff
Maksym Nezhurin <mnezhurin17@gmail.com>
2024-10-13 14:35:07 +0200

    prevent cors 1

--- server.mjs
+++ server.mjs
@@ -18,7 +18,9 @@ const router = express.Router();
 // Connect to DB 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://maksym-nezhurin.github.io'
+}));
 // Middleware
 app.use(helmet());
 // app.use((res, req, next) => {
@@ -54,10 +56,10 @@ const server = http.createServer(app);
 
 // Initialize Socket.IO
 const io = new Server(server, {
-    // cors: {
-    //     origin: 'https://maksym-nezhurin.github.io/react-app-test-crud-api-app/', // Frontend URL
-    //     methods: ['GET', 'POST'],
-    // },
+    cors: {
+        origin: 'https://maksym-nezhurin.github.io',
+        methods: ['GET', 'POST'],
+    },
   });
 
 io.on('connection', (socket) => {
