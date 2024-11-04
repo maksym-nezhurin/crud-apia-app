169168c03933ca434e872d1aba70aff66e5d1b5e
Maksym Nezhurin <mnezhurin17@gmail.com>
2024-11-04 19:58:31 +0100

    Create reset password flow

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,14 @@
 import express from 'express';
-import { registerUser, getUserDetails, refreshToken, loginUser, logoutUser } from '../controllers/userController.mjs';
+import {
+    registerUser,
+    getUserDetails,
+    refreshToken,
+    loginUser,
+    logoutUser,
+    resetPassword,
+    requestPasswordResetCode,
+    verifyResetCode
+} from '../controllers/userController.mjs';
 import auth from '../middleware/auth.mjs';
 
 const router = express.Router();
@@ -8,6 +17,9 @@ router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/logout', logoutUser);
 router.post('/refreshToken', refreshToken);
+router.post('/reset-password', resetPassword);
+router.post('/request-password-reset-code', requestPasswordResetCode);
+router.post('/verify-reset-code', verifyResetCode);
 router.get('/me', auth, getUserDetails);
 
 export default router;
