3ee288b47f79f1ad9f49598ad813bc79ea29dd66
Maksym Nezhurin <mnezhurin17@gmail.com>
2024-11-28 21:49:38 +0100

    fixes

--- controllers/paymentController.mjs
+++ controllers/paymentController.mjs
@@ -1,7 +1,7 @@
 import Stripe from 'stripe';
 import Joi from 'joi'; // Install with npm install joi
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2024-11-20.acacia' });
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const paymentSchema = Joi.object({
     paymentMethodId: Joi.string().required(),
