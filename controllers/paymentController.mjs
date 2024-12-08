5b64a9465322150931fc75f1074bf84e1032d127
Maksym Nezhurin <mnezhurin17@gmail.com>
2024-12-08 13:04:20 +0100

    [stripe] - fix stripe workflow

--- controllers/paymentController.mjs
+++ controllers/paymentController.mjs
@@ -1,5 +1,6 @@
 import Stripe from 'stripe';
 import Joi from 'joi'; // Install with npm install joi
+import axios from "axios";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
@@ -20,16 +21,44 @@ export const checkoutUserData = async (req, res) => {
     }});
 }
 
+const convertValueToCurrency = async (amount, currency = 'PLN') => {
+    try {
+        const response = await axios.get(process.env.EXCHANGE_API_URL + '/USD');
+        const rates = response.data.rates;
+        const usdToPlnRate = rates[currency];
+
+        if (!usdToPlnRate) {
+            throw new Error(`${currency} rate not available`);
+        }
+
+        // Perform conversion
+        const amountInPLN = amount * usdToPlnRate;
+
+        return {
+            success: true,
+            amountInPLN,
+            rate: usdToPlnRate,
+        };
+    } catch (error) {
+        console.error('Error converting USD to PLN:', error.message);
+        return {
+            success: false,
+            error: error.message,
+        };
+    }
+}
+
 export const createPayment = async (req, res) => {
     const { error, value } = paymentSchema.validate(req.body);
     if (error) {
         return res.status(400).json({ error: error.details[0].message });
     }
     const { paymentMethodId, amount } = value;
+    const { amountInPLN } = await convertValueToCurrency(amount);
 
     try {
         const paymentIntent = await stripe.paymentIntents.create({
-            amount: amount, // Convert dollars to cents
+            amount: amountInPLN * 100, // Convert dollars to cents
             currency: 'pln',
             payment_method: paymentMethodId,
             // confirmation_method: 'manual',
@@ -39,7 +68,10 @@ export const createPayment = async (req, res) => {
             },
             // confirm: true,
         });
-        res.json({ clientSecret: paymentIntent.client_secret });
+
+        res.json({
+            data: { clientSecret: paymentIntent.client_secret }
+        });
     } catch (error) {
         console.error('Error creating payment intent:', error);
         res.status(500).json({ error: error.message });
